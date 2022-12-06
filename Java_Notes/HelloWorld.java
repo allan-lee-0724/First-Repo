52f4ec15ad8fab1898e2090a36b3b6a06e746b2e
allan-lee-0724 <allan841@revature.net>
2022-12-06 11:54:02 -0500

    adding initial Java notes and images to repo

---
+++ Java_Notes/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+
+        /*
+         * If you want to compile and execute this file manually you would do the following:
+         * 1. Run the command javac HelloWorld.java to compile the source code into byte code
+         * 2. Run the command java HelloWorld to execute the application
+         */
+    }
+}
