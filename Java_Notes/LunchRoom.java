b8a2e835b9d64c467181ee2a47fc3585c3b55af4
allan-lee-0724 <allan841@revature.net>
2022-12-07 12:24:21 -0500

    updated LunchRoom

--- Java_Notes/LunchRoom.java
+++ Java_Notes/LunchRoom.java
@@ -2,6 +2,8 @@ public class LunchRoom {
     public static void main(String[] args) {
         // Both options below are perfectly valid ways of creating Burrito and Croissant objects
         // We can reference their parent class, Food, as the type, and then make the spectific type of food object
+        // BE AWARE: if you set the type to the parent but define child specific method or field you will NOT have 
+        // access to it if the type is of the parent (myBurrito loses access to the addSourCream method if its type is Food)
         Burrito myBurrito = new Burrito();
         Food myCroissant = new Croissant();
 
